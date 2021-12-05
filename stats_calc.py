a490807ad0a9380c99192dfdb8cab3267e926e38
Heinrich Harms <freelanceharms@gmail.com>
2021-12-05 13:04:40 -0600

    updates

--- stats_calc.py
+++ stats_calc.py
@@ -1,14 +1,22 @@
-from cv2 import VideoCapture, CAP_PROP_FRAME_COUNT, dct, split, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FRAME_WIDTH
+from cv2 import (
+    VideoCapture,
+    dct,
+    split,
+    CAP_PROP_FRAME_COUNT,
+    CAP_PROP_FRAME_HEIGHT,
+    CAP_PROP_FRAME_WIDTH
+)
+from numpy import int32, float32
+
 from itertools import product
 from os.path import join
 from os import listdir
-from numpy import int32, float32
 from json import dump
 
 
 VIDEOS_DIR = "videos"
-DCT_COEFFICIENT_1 = 1, 1 # main coefficient
-DCT_COEFFICIENT_2 = 0, 0 # secondary coefficient
+DCT_COEFFICIENT_1 = 2, 2 # primary coefficient
+DCT_COEFFICIENT_2 = 1, 1 # secondary coefficient
 
 
 if __name__ == "__main__":
@@ -38,76 +46,92 @@ if __name__ == "__main__":
     variation_hists = [[0] * 2048, [0] * 2048, [0] * 2048]
     block_count = 0
 
+    # coefficient value logging variables
+    lowest = 1e9
+    highest = -1e9
+
     # correlation logging variables
     correlation_heatmap = [[[0] * 2048 for _j in range(2048)] for _i in range(3)]
 
     # iterate over video frames
     for frame_i in range(n):
 
-        # read next frames from video capture objects
-        reads = [video.read() for video in videos]
-        rets = [i[0] for i in reads] # ugly
-        frames = [i[1] for i in reads] # ugly
+        try:
+            # read next frames from video capture objects
+            reads = [video.read() for video in videos]
+            rets = [i[0] for i in reads] # ugly
+            frames = [i[1] for i in reads] # ugly
 
-        # if last frame, break read loop
-        if rets.count(False): break
+            # if last frame, break read loop
+            if rets.count(False): break
 
-        # split frames into channel arrays
-        channels = [split(frame) for frame in frames]
+            # split frames into channel arrays
+            channels = [split(frame) for frame in frames]
 
-        # iterate over 8x8 blocks and channel indexes
-        for x, y in product(
-            range(0, frame_shape[1], 8),
-            range(0, frame_shape[0], 8)
-        ):
-            # calculate dct coefficients for current block of each frame
-            x_upper = x + 8 if x + 8 < frame_shape[1] else None
-            y_upper = y + 8 if y + 8 < frame_shape[0] else None
-            dct_coefficients = [
-                [
-                    int32(dct(
-                        float32(frame_channels[i][y: y_upper, x: x_upper])
-                    )).tolist()
-                    for i in range(3)
+            # iterate over 8x8 blocks and channel indexes
+            for x, y in product(
+                range(0, frame_shape[1], 8),
+                range(0, frame_shape[0], 8)
+            ):
+                # calculate dct coefficients for current block of each frame
+                x_upper = x + 8 if x + 8 < frame_shape[1] else None
+                y_upper = y + 8 if y + 8 < frame_shape[0] else None
+                dct_coefficients = [
+                    [
+                        int32(dct(
+                            float32(frame_channels[i][y: y_upper, x: x_upper])
+                        )).tolist()
+                        for i in range(3)
+                    ]
+                    for frame_channels in channels
                 ]
-                for frame_channels in channels
-            ]
 
-            # calculate variation of DCT_COEFFICIENT_1 in current block
-            variations = [
-                abs(max(
-                    dct_coefficients,
-                    key=lambda x: x[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
-                )[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]] - min(
-                    dct_coefficients,
-                    key=lambda x: x[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
-                )[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]][0])
-                for channel_index in range(3)
-            ]
+                # calculate variation of DCT_COEFFICIENT_1 in current block
+                variations = [
+                    abs(max(
+                        dct_coefficients,
+                        key=lambda x: x[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
+                    )[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]] - min(
+                        dct_coefficients,
+                        key=lambda x: x[channel_index][DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
+                    )[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]][0])
+                    for channel_index in range(3)
+                ]
 
-            # update variation logging variables
-            for i, variation in enumerate(variations):
-                if variation < lowest_variation[i]:
-                    lowest_variation[i] = variation
-                if variation > highest_variation[i]:
-                    highest_variation[i] = variation
-                variation_totals[i] += variation
-                variation_hists[i][variation] += 1
-            block_count += 1
+                # update variation logging variables
+                for i, variation in enumerate(variations):
+                    if variation < lowest_variation[i]:
+                        lowest_variation[i] = variation
+                    if variation > highest_variation[i]:
+                        highest_variation[i] = variation
+                    variation_totals[i] += variation
+                    variation_hists[i][variation] += 1
+                block_count += 1
 
-            # update correlation logging variables
-            for dct_blocks in dct_coefficients:
-                for channel_index, channel_block in enumerate(dct_blocks):
-                    correlation_heatmap[channel_index][1024 - channel_block[DCT_COEFFICIENT_2[0]][DCT_COEFFICIENT_2[1]]][1024 - channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]] += 1
-        print(f"Progress {frame_i / n * 100:.2f}%")
+                # update correlation and coefficient logging variables
+                for dct_blocks in dct_coefficients:
+                    for channel_index, channel_block in enumerate(dct_blocks):
 
+                        correlation_heatmap[channel_index][1024 - channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]][channel_block[DCT_COEFFICIENT_2[0]][DCT_COEFFICIENT_2[1]]] += 1
 
-    with open("stats.json", "w") as output_file:
+                        if channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]] < lowest:
+                            lowest = channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
+                        if channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]] > highest:
+                            highest = channel_block[DCT_COEFFICIENT_1[0]][DCT_COEFFICIENT_1[1]]
+
+            print(f"Progress {frame_i / n * 100:.2f}%")
+
+        except KeyboardInterrupt:
+            break
+
+    with open("stats_2_2_1_1_small.json", "w") as output_file:
         dump({
             "lowest_variation": lowest_variation,
             "highest_variation": highest_variation,
             "variation_totals": variation_totals,
             "variation_hists": variation_hists,
+            "lowest_val": lowest,
+            "highest_val": highest,
             "block_count": block_count,
             "function_of_dct_2_hists": correlation_heatmap
         }, output_file)
